071c7d3957dc381c5c76d009353fb27fd9b275d2
74ls00 <userled@yandex.ru>
2020-10-19 19:56:07 +0300

    .

--- FW/B0LCDFW/fonts/bdfconv222.bat
+++ FW/B0LCDFW/fonts/bdfconv222.bat
@@ -1,29 +1,19 @@
 cls
 set "bdfconv_2_22=%~d0%~p0bdfconv\bdfconv_2_22.exe"
-
-rem set font_name=u8g2_font_18d
-rem set font_name=u8g2_font_7d
-rem set font_name=u8g_font_04b_03b
-
+rem
 set font_name=u8g2_font_7x13B
 
 
-set "file_c=%~d0%~p0%font_name%.h"
-del "%file_c%"
-
-rem set "font_bdf=G:\home\Documents\Projects\0-Electronics\E-bike\0-Display\LCD12864_ST7920_u8g2\pTest_g2_v2\font_arduino-1.8.13p\ncenB18m.bdf"
-rem set "font_bdf=G:\home\Documents\Projects\0-Electronics\E-bike\0-Display\LCD12864_ST7920_u8g2\pTest_g2_v2\font_arduino-1.8.13p\u8g2_font_7d.bdf"
-set "font_bdf=G:\home\Documents\Projects\0-Electronics\E-bike\0-Display\LCD12864_ST7920_u8g2\pTest_g2_v2\font_arduino-1.8.13p\%font_name%.bdf"
 
 
-
-
+rem
+del "%~d0%~p0%font_name%.h"
 
 rem 46-102
 rem %bdfconv_2_22% -v -f 1 -m "46-58" %font_bdf% -o %file_c%  -n %font_name%
 rem u8g_font_04b_03b 46-86 
 rem u8g2_font_7x13B num % 25-39 37-57
-%bdfconv_2_22% -v -f 1 -m "37-57" %font_bdf% -o %file_c%  -n %font_name%
+%bdfconv_2_22% -v -f 1 -m "37-57" "%~d0%~p0%font_name%.bdf" -o "%~d0%~p0%font_name%.h"  -n %font_name%
 
 
 rem pause

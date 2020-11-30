6685f89d50d73c87e6e9777c46f093d31acbb368
74ls00 <userled@yandex.ru>
2020-11-30 03:22:19 +0300

    .

---
+++ FW/B0T13/B0T13.ino
@@ -0,0 +1,19 @@
+// MicroCore ATtiny13A  https://github.com/MCUdude/MicroCore#how-to-install
+// ATtiny13 9.6Mhz int. 
+
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(0, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+
+
+analogWrite(0,1 );
+
+
+  
+}
